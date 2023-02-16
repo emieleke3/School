63cc5b75c3084fe4332253a8f6f0ed0db36a5c28
Emiel Vreemann <118814872+emieleke3@users.noreply.github.com>
2023-02-16 10:57:35 +0100

    yes

--- OOP/Week1/Exercise3/src/nl/saxion/oop/week1/Application.java
+++ OOP/Week1/Exercise3/src/nl/saxion/oop/week1/Application.java
@@ -10,6 +10,43 @@ public class Application implements Runnable {
     }
 
     public void run() {
-        ArrayList<Car> cars = new ArrayList<>();
+        String type = "Toyota";
+        String numberPlate = "(bla-32-1)";
+        double tankSize = 50.0;
+        double consumption = 8.0;
+        double currentTank = 50.0;
+        int kilometers = 0;
+
+        Car car = new Car(type, numberPlate, tankSize, currentTank, consumption, kilometers );
+
+        int menuInput = -1;
+
+        do {
+           SaxionApp.printLine("*********************************");
+           SaxionApp.printLine("1) Display statistics of the car");
+           SaxionApp.printLine("2) Drive some kilometers");
+           SaxionApp.printLine("3) Refuel");
+           SaxionApp.printLine("0) Exit program");
+           SaxionApp.printLine("*********************************");
+           menuInput = SaxionApp.readInt();
+
+           if (menuInput == 1) {
+                car.toStringCar();
+                SaxionApp.pause();
+                SaxionApp.clear();
+           }
+           if (menuInput == 2) {
+                car.drive();
+                SaxionApp.pause();
+                SaxionApp.clear();
+           }
+           if (menuInput == 3) {
+                car.fuel();
+                SaxionApp.pause();
+                SaxionApp.clear();
+           }
+
+        } while (menuInput != 0);
     }
+
 }
--- OOP/Week1/Exercise3/src/nl/saxion/oop/week1/Car.java
+++ OOP/Week1/Exercise3/src/nl/saxion/oop/week1/Car.java
@@ -1,22 +1,52 @@
 package nl.saxion.oop.week1;
 
+import nl.saxion.app.SaxionApp;
+
 public class Car {
     private String type;
     private String numberPlate;
-    private int tankSize;
-    private int consumption;
+    private double tankSize;
+    private double currentTank;
+    private double consumption;
+    private int kilometers;
 
-    public Car (String type, String numberPlate, int tankSize, int consumption) {
+    public Car (String type, String numberPlate, double tankSize, double currentTank, double consumption, int kilometers) {
         this.type = type;
         this.numberPlate = numberPlate;
         this.tankSize = tankSize;
+        this.currentTank = currentTank;
         this.consumption = consumption;
+        this.kilometers = kilometers;
     }
 
+    public void toStringCar() {
+        SaxionApp.printLine(type + numberPlate + ", " + currentTank + " litres " + kilometers);
+    }
     public void drive() {
+        SaxionApp.print("How many kilometers are you going to drive? ");
+        int amount = SaxionApp.readInt();
 
+        double calculation = 1 / consumption * amount;
+
+        if (calculation >= currentTank) {
+            SaxionApp.printLine("You cant drive that far!");
+        } else {
+            double amountLeft = currentTank - calculation;
+            currentTank = amountLeft;
+            SaxionApp.printLine("You have " + amountLeft + " in your tank");
+        }
     }
     public void fuel() {
+            SaxionApp.printLine("How much liters do you want?");
+            double amount = SaxionApp.readInt();
 
+            double refilledAmount = currentTank + amount;
+
+            if (refilledAmount > tankSize) {
+                SaxionApp.printLine("You cant add that many liters");
+            } else {
+                currentTank = refilledAmount;
+                SaxionApp.printLine("Your tank is now: " + currentTank);
+            }
     }
 }
